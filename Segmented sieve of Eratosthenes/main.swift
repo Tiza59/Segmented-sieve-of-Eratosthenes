d56a8c596778d27951da9f778a3895c53267d1f9
Tiza <uwefalck@mac.com>
2019-11-03 12:53:17 +0100

    Initial Commit

---
+++ Segmented sieve of Eratosthenes/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Segmented sieve of Eratosthenes
+//
+//  Created by Uwe Falck on 03.11.19.
+//  Copyright © 2019 Uwe Falck. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
